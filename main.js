de30416b7560a32eb8a2ed9b4ef5a3732c710ffe
Paul-Berth <paulb3656@gmail.com>
2022-10-26 12:30:50 +0200

    address and time

--- main.js
+++ main.js
@@ -6,15 +6,29 @@ function cleanStr(str) {
 
 function regexHtml(tag, classAttr) {
   return new RegExp(
-    `<${tag}[^>]*?class="[^"]*?${classAttr}[^"]*?"[^>]*?>(.*?)</${tag}>`,
+    `<${tag}[^>]*?class="[^"]*?(?:${classAttr.join(
+      '|'
+    )})[^"]*?"[^>]*?>(.*?)</${tag}>`,
     'gi'
   );
 }
 
 function parseSample(sample) {
+  const results = {
+    arrivalAddress: '',
+    arrivalTime: '',
+    departureAddress: '',
+    departureTime: '',
+  };
+
+  const regexTime = /(\d+:\d+)/gi;
   const regexComments = /<!--.*?-->/gi;
   const regexStyle = /<style.*?>.*?<\/style>/gi;
-  const regexAddress = regexHtml('span', 'add');
+  const regexInnerHtmlStart = /^([^<]+?)/gi;
+  const regexInnerHtmlEnd = /([^>]+?)$/gi;
+  const regexTdAddress = regexHtml('td', ['time', '\\baddress\\b']);
+  const regexSpanTime = regexHtml('span', ['time']);
+  const regexSpanAddress = regexHtml('span', ['address']);
 
   // 1 -- CLEAN (remove comments and css)
   let html = sample.html.replace(regexComments, '');
@@ -25,31 +39,71 @@ function parseSample(sample) {
     }
   });
 
-  // 2 -- ADDRESS (address tag)
-  const results = [...html.matchAll(regexAddress)];
-  console.log('results.length = ', results.length);
-  console.log(results.map((e) => [e[0], cleanStr(e[1]), e.index]));
+  // 2 -- ADDRESS && TIME (td address && time)
+  const tdAddress = [...html.matchAll(regexTdAddress)];
+  // console.log('results.length = ', tdAddress.length);
+  // console.log(tdAddress.map((e) => [e[0], cleanStr(e[1]), e.index]));
+  for (let i = 0; i < tdAddress.length; i++) {
+    const isDeparture =
+      results.departureTime == '' && results.departureAddress == '';
+    const innerTd = tdAddress?.[i]?.[1] || '';
 
-  // 3 -- TIME (time tag)
+    // Get innerHTML
+    const innerHtmlStart = cleanStr(
+      [...innerTd.matchAll(regexInnerHtmlStart)]?.[0]?.[1] || ''
+    );
+    const innerHtmlEnd = cleanStr(
+      [...innerTd.matchAll(regexInnerHtmlEnd)]?.[0]?.[1]?.trim() || ''
+    );
+    innerHtmlStart != ''
+      ? (timeOrAddress = innerHtmlStart)
+      : (timeOrAddress = innerHtmlEnd);
+    let timeInnerHtml = timeOrAddress.match(regexTime)?.[0] || '';
+    isDeparture
+      ? timeInnerHtml == ''
+        ? (results.departureAddress = timeOrAddress)
+        : (results.departureTime = timeInnerHtml)
+      : timeInnerHtml == ''
+      ? (results.arrivalAddress = timeOrAddress)
+      : (results.arrivalTime = timeInnerHtml);
 
-  // 4 -- CURRENCY (price tag)
+    // Get Span with time tag
+    const spanTime = [...innerTd.matchAll(regexSpanTime)];
+    let time = cleanStr(spanTime?.[0]?.[1] || '');
+    time = time.match(regexTime)?.[0] || '';
+    time != '' &&
+      (isDeparture
+        ? (results.departureTime = time)
+        : (results.arrivalTime = time));
 
-  // 5 -- DISTANCE (distance tag)
+    // Get Span with address tag
+    const spanAddress = [...innerTd.matchAll(regexSpanAddress)];
+    const address = cleanStr(spanAddress?.[0]?.[1] || '');
+    address != '' &&
+      (isDeparture
+        ? (results.departureAddress = address)
+        : (results.arrivalAddress = address));
+  }
 
-  // 6 -- DISTANCEFEE (distance tag)
+  // 2 -- DISTANCE (distance tag)
 
-  // 7 -- DISTANCEUNIT (distance tag)
+  // 3 -- DISTANCEUNIT (distance tag)
 
-  // 8 -- DURATION ('durée' in text)
+  // 4 -- DURATION ('durée' in text)
 
-  // 9 -- TIMEFEE ('temps' in text && price tag)
+  // 5 -- DISTANCEFEE (distance tag)
 
-  // 10 -- TOTALPRICEPAID (price final-charge | totalPrice tag)
+  // 6 -- TIMEFEE ('temps' in text && price tag)
 
+  // 7 -- TOTALPRICEPAID (price final-charge | totalPrice tag)
+
+  // 8 -- CURRENCY (price tag)
+
+  console.log(results);
   return {};
 }
 
-const sample1 = require('./samples/sample_test').default;
+const sample1 = require('./samples/sample_3').default;
 parseSample(sample1);
 
 exports.parseSample = parseSample;
